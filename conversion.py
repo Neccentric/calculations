c868847e3272aceb34467df2c776f06f4f13299b
QuantumTech-droid <obiscondu@gmail.com>
2024-03-18 23:19:24 +0100

    Finding the Average of two numbers

--- conversion.py
+++ conversion.py
@@ -6,3 +6,15 @@ def CelsiustoFah():
     print(conversion)
 CelsiustoFah()
 
+
+# Defining a function that takes two arguemets and returns their average.
+def Average():
+    print("Enter Arguement 1: ")
+    arguement1 = int(input())
+    print("Enter Arguement 2: ")
+    arguement2 = int(input())
+    averge_of_arg1_arg2 = (arguement1 + arguement2) / 2
+    print(f"The Average of {arguement1} and {arguement2} is {averge_of_arg1_arg2}")
+Average()
+# Note that an Arguement refers to a value that you pass to a Function when calling it.
+# Arguements are specified within the parentheses following the Function and it can be of various types.
